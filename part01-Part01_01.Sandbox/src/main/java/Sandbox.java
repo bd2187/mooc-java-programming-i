a7f4e9f6c06501157d3ccb6b249e231b82d12a31
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-07 19:52:20 -0700

    print user input

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,7 +1,21 @@
+// Import scanner tool for reading user input
+import java.util.Scanner;
 
 public class Sandbox {
 
     public static void main(String[] args) {
-        System.out.println("hello, world!!!");                
+        // Create a tool for scanning user input. Name it Scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        // Print "Write a message"
+        System.out.println("Write a message: ");
+        
+        // Read the string written by the user, and
+        // assign it to program memory "String message = (string that was given as input)"
+        String message = scanner.nextLine();
+        
+        // Print the message written by the user
+        System.out.println(message);
+        
     }
 }
