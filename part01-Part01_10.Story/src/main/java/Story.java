59378a73b96aa58e462a313e0d851a9f59a6259f
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-08 18:42:42 -0700

    complete story challenge

--- part01-Part01_10.Story/src/main/java/Story.java
+++ part01-Part01_10.Story/src/main/java/Story.java
@@ -1,3 +1,15 @@
+/*
+I will tell you a story, but I need some information first.
+What is the main character called?
+    Bob
+What is their job?
+    a builder
+Here is the story:
+Once upon a time there was Bob, who was a builder.
+On the way to work, Bob reflected on life.
+Perhaps Bob will not be a builder forever.
+*/
+
 
 import java.util.Scanner;
 
@@ -7,6 +19,18 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("I will tell you a story, but I need some information first.");
+        
+        System.out.println("What is the main character called?");
+        String name = scanner.nextLine();
+        
+        System.out.println("What is their job?");
+        String occupation = scanner.nextLine();
+        
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + name + ", who was " + occupation + ".");
+        System.out.println("On the way to work, " + name + " reflected on life.");
+        System.out.println("Perhaps " + name + " will not be " + occupation + " forever.");
 
     }
 }
