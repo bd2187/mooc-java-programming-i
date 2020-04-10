29800f15fa95ecdd56fc43af947be24801a9c9ce
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-09 20:56:10 -0700

    complete SumOfTwoNumbers challenge

--- part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -1,4 +1,12 @@
+/*
+    Write a program that asks the user for two numbers. After this, the program prints the sum of the numbers given by the user.
 
+    Give the first number:
+        8
+    Give the second number:
+        3
+    The sum of the numbers is 11
+*/
 import java.util.Scanner;
 
 public class SumOfTwoNumbers {
@@ -6,7 +14,13 @@ public class SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("The sum of the numbers is " + (firstNum + secondNum));
 
     }
 }
