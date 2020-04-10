3388a8cf93c41b16f520d550c2ee52d2066eebd0
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-09 20:44:49 -0700

    complete secondsInAday

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -1,3 +1,9 @@
+/*
+How many days would you like to convert to seconds?
+    1
+    86400
+*/
+
 
 import java.util.Scanner;
 
@@ -6,7 +12,16 @@ public class SecondsInADay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        
+        int userInput = Integer.valueOf(scanner.nextLine());
+        
+        // 1440 minutes in a day
+        // 60 seconds 
+        
+        int totalMinutes = userInput * 1440;
+        int totalSeconds = totalMinutes * 60;
+        System.out.println(totalSeconds);
 
     }
 }
