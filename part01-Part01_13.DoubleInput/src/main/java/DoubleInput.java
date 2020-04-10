f7d3f19b56873ad1e5098dc69e60f1c5c3d3af2b
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-09 20:14:24 -0700

    complete doubleInput

--- part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -6,7 +6,14 @@ public class DoubleInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
+        System.out.println("Give a number:");
+        
+        String userInput = scanner.nextLine();
+        
+        double dub = Double.valueOf(userInput);
+        
+        System.out.println("You gave the number " + dub);
+        
 
     }
 }
