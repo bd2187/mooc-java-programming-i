d34d589ac59a3005cb3853536b2c8f26c5822d7d
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-14 14:43:09 -0700

    Complete GradesAndPoints

--- part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -1,3 +1,14 @@
+/*
+    < 0	impossible!
+    0-49	failed
+    50-59	1
+    60-69	2
+    70-79	3
+    80-89	4
+    90-100	5
+    > 100	incredible!
+*/
+
 
 import java.util.Scanner;
 
@@ -5,6 +16,41 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        System.out.println("Give points [0-100]:");
+        int points = Integer.valueOf(scan.nextLine());
+                
+        if(points < 0) {
+            
+            System.out.println("Grade: impossible!");
+            
+        } else if(points <= 49) {
+            
+            System.out.println("Grade: failed");
+            
+        } else if(points <= 59) {
+            
+            System.out.println("Grade: 1");
+            
+        } else if(points <= 69) {
+            
+            System.out.println("Grade: 2");
+            
+        } else if(points <= 79) {
+            
+            System.out.println("Grade: 3");
+            
+        } else if(points <= 89) {
+            
+            System.out.println("Grade: 4");
+            
+        } else if(points <= 100) {
+            
+            System.out.println("Grade: 5");
+            
+        } else {
+            
+            System.out.println("incredible!");
+        }
     }
 }
