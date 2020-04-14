745136d222fdd9b69e77e5df8e56da99bf24fdbe
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-14 15:36:15 -0700

    Complete CheckingTheAge challenge

--- part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -5,6 +5,13 @@ public class CheckingTheAge {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("How old are you?");
+        int num = Integer.valueOf(scan.nextLine());
+        
+        if(num < 0 || num > 120 ) {
+            System.out.println("Impossible!");
+        } else {
+            System.out.println("OK");
+        }
     }
 }
