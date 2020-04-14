d04387d6241a4eb50e16009be14a7993eba20b52
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-14 14:12:07 -0700

    Complete LargerThanOrEqualTo challenge

--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -5,6 +5,26 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scan.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scan.nextLine());
+        
+        if(firstNum > secondNum) {
+            
+            System.out.println("Greater number is " + firstNum);
+            
+        } else if(secondNum > firstNum) {
+            
+            System.out.println("Greater number is " + secondNum);
+
+        } else if(firstNum == secondNum) {
+            
+            System.out.println("The numbers are equal!");
+            
+        }
 
     }
 }
