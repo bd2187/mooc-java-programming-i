0333446634ec7196c45a7973467a7bd3aa7498cb
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-14 13:12:24 -0700

    complete simplecalculator

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -5,8 +5,23 @@ public class SimpleCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+        
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        int sum = firstNum + secondNum;
+        int diff = firstNum - secondNum;
+        int product = firstNum * secondNum;
+        double div = (double) firstNum / secondNum;
+        
+        System.out.println(firstNum + " + " + secondNum + " = " + sum);
+        System.out.println(firstNum + " - " + secondNum + " = " + diff);
+        System.out.println(firstNum + " * " + secondNum + " = " + product);
+        System.out.println(firstNum + " / " + secondNum + " = " + div);
+        
 
     }
 }
