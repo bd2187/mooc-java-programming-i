99a10c5e6187d4eccee945a5c130f342dc2ab823
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-14 21:25:02 -0700

    Complete squarerootofsum

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -1,10 +1,21 @@
-
+/*
+    Write a program that reads two integers from the user and prints the square root
+    of the sum of these integers. The program does not need to work with negative
+    values.
+*/
 import java.util.Scanner;
 
 public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        double res = Math.sqrt(firstNum + secondNum);
+        
+        System.out.println(res);
+        
 
     }
 }
