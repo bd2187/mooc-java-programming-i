5fbdbdee58e729053c20fe84f516b06fc8b52c45
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-15 15:52:06 -0700

    complete CarryOn

--- part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -5,6 +5,16 @@ public class CarryOn {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            System.out.println("Carry on?");
+            String res = scanner.nextLine();
+            
+            if(res.equals("no")) {
+                break;
+            }
+            
+        }       
 
     }
 }
