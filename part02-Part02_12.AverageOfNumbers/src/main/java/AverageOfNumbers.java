3c4b35aeb4c2641af7ff21123d5c46c925c52fc3
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-16 18:47:21 -0700

    complete averageofnumbers

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,10 +1,35 @@
 
+/*
+Write a program that asks the user for input until the user inputs 0. After this, the program prints the average of the numbers. The number zero does not need to be counted to the average. You may assume that the user inputs at least one number.
+
+The average of the numbers can be calculated by dividing the sum of numbers with the amount of the numbers
+*/
+
 import java.util.Scanner;
 
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int count = 0;
+        int sum = 0;
+        while(true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == 0) {
+                break;
+            } else {
+                count++;
+                sum += input;
+            }                      
+            
+        }
+        
+        double avg = (double) sum / count;
+        
+        System.out.println("Average of the numbers: " + avg);
 
     }
 }
