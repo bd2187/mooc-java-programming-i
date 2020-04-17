fac009a3bb19af644de0ffa7f0e33b7d3dac33dc
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-16 18:05:14 -0700

    Complete NumberOfNegativeNumbers

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -1,3 +1,9 @@
+/*
+    Write a program that reads values from the user until they input a 0.
+    After this, the program prints the total number of inputted values that are negative.
+    The zero that's used to exit the loop should not be included in the total number count.
+*/
+
 
 import java.util.Scanner;
 
@@ -5,6 +11,25 @@ public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int count = 0;
+        
+        while(true) {
+            
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == 0) {
+                break;
+                
+            } else if(input < 0) {
+                
+                count++;
+                
+            }
+        }
+        
+        System.out.println("Number of negative numbers: " + count);
 
     }
 }
