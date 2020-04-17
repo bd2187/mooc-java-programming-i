89cc87d0e1fa0dc8d41d0beaf86d752c4f6251cc
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-16 18:59:12 -0700

    Complete AverageofPositiveNumbers

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,42 @@
+/*
+    Write a program that asks the user for input until the user inputs 0.
+    After this, the program prints the average of the positive numbers
+    (numbers that are greater than zero).
 
+    If no positive number is inputted, the program prints "Cannot calculate the average"
+*/
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int count = 0;
+        int sum = 0;
+        
+        while(true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == 0) {
+                
+                break;
+                
+            } else if (input > 0){
+                
+                count++;
+                sum += input;
+                
+            }
+        }
+        
+        if(count == 0 ) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            double avg = (double) sum / count;
+            System.out.println(avg);
+        }
 
     }
 }
