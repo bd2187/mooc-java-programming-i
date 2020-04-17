5db613e71152ba782bc4f0323e4b5a44314868d1
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-16 18:12:31 -0700

    Complete sumofnumbers

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -1,3 +1,10 @@
+/*
+    Write a program that reads numbers from the user until the user inputs a number 0.
+    After this the program outputs the sum of the numbers.
+    The number zero does not need to be added to the sum,
+    even if it does not change the results.
+*/
+
 
 import java.util.Scanner;
 
@@ -5,6 +12,23 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+        
+        while(true) {
+            
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            
+            if(num == 0) {
+                break;
+            } else {
+                sum += num;
+            }
+            
+        }
+        
+        System.out.println("Sum of the numbers: " + sum);
 
     }
 }
