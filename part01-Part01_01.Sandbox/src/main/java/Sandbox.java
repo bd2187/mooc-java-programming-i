7d6299d5f709964ce01fc3082bb7b57aeb6cdce7
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-16 18:00:56 -0700

    Complete NumberOfNumbers

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -6,22 +6,29 @@ public class Sandbox {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int number = 0;
-
         while (true) {
-            number = number + 1;
 
-            if (number >= 5) {
+            // The task is to keep count of number ones
+            int ones = 0;
+
+            System.out.println("Insert a number (0 exits): ");
+            // The task is to read a user inputted number
+            int number = Integer.valueOf(scanner.nextLine());
+
+            // The task is to exit the loop if the user
+            // has inputted zero
+            if (number == 0) {
                 break;
             }
 
-            if (number < 5) {
-                continue;
+            // The task is to increase the amount of ones
+            // if the user inputs a number one
+            if (number == 1) {
+                ones = ones + 1;
             }
 
-            System.out.print(number + " ");
+            // The task is to print out the total of ones
+            System.out.println("The total of ones: " + ones);
         }
-
-        System.out.print(number + " ");
     }
 }
