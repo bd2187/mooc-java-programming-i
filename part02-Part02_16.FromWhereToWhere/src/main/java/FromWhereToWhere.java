80795a521cbc9d924cc54330f09481493a961dac
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-18 11:50:27 -0700

    Complete fromwheretowhere

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,18 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Where to?");
+        int dest = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Where from?");
+        int start = Integer.valueOf(scanner.nextLine());
+        
+        if(dest > start) {
+            for(int i = start; i <= dest; i++) {
+                System.out.println(i);
+            }            
+        }
+        
+
     }
 }
