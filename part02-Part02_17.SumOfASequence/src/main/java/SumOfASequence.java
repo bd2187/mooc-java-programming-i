fd6815762222c0f7c9849d875bd609b129a966d0
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-18 11:59:43 -0700

    Complete SumOfASequence

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -1,10 +1,21 @@
-
+/*
+    Implement a program, which calculates the sum 1+2+3+...+n where n is given as user input.
+*/
 import java.util.Scanner;
 
 public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Last number?");
+        int num = Integer.valueOf(scanner.nextLine());
+        
+        int sum = 0;
+        for(int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        
+        System.out.println(sum);
     }
 }
