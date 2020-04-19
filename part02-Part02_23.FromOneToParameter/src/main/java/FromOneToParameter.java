f263849af4a335fa34a900747aaa1f7e06d7b1a9
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-19 10:24:53 -0700

    Complete FromOneToParameter

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,20 @@
-
+/*
+    Create the following method in the exercise template: public static void
+    printUntilNumber(int number). It should print the numbers from one
+    to the number passed as a parameter.
+    Two examples of the method's usage are given below.
+*/
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        printUntilNumber(2);
+    }
+    
+    public static void printUntilNumber(int number) {
+        for(int i = 1; i <= number; i++) {
+            System.out.println(i);
+        }
     }
 
 }
