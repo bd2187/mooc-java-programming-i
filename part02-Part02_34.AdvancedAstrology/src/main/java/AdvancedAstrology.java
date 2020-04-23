5bf1363772551a91b6a99e5959b93c1816dd8dba
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-22 18:14:39 -0700

    Complete AdvancedAstrology

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,23 +3,52 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+        while(number > 0) {
+            System.out.print("*");
+            number--;
+        }
+        
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        while(number > 0) {
+            System.out.print(" ");
+            number--;
+        }
     }
 
     public static void printTriangle(int size) {
-        // part 2 of the exercise
+        
+        for(int i = 0; i < size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
+        
     }
 
     public static void christmasTree(int height) {
+        
+        int starsCounter = 1;
         // part 3 of the exercise
+        for(int i = 0; i < height; i++) {
+            printSpaces(height - i);
+            printStars(starsCounter);
+            starsCounter += 2;
+        }
+        
+        printSpaces(height -1);
+        printStars(3);
+        printSpaces(height -1);
+        printStars(3);
+        
+        
     }
 
     public static void main(String[] args) {
         // The tests are not checking the main, so you can modify it freely.
-
+        
         printTriangle(5);
         System.out.println("---");
         christmasTree(4);
