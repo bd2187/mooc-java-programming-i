c00a8cf9937b68c082f70df63b93fac406b57584
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-23 18:25:07 -0700

    Complete RememberTheseNumbers

--- part03-Part03_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
+++ part03-Part03_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
@@ -1,21 +1,27 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RememberTheseNumbers {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int luku = Integer.valueOf(scanner.nextLine());
-            if (luku == -1) {
+        
+        while(true) {
+            Integer input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == -1) {
                 break;
+            } else {
+                numbers.add(input);
             }
-
-            numbers.add(luku);
+            
         }
-
+        
+        for(int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+        
+        
     }
 }
