7816a48b91254dfaeacff7bc0aa4e91b7b2ff643
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-23 18:36:37 -0700

    Complete OnlyTheseNumbers

--- part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
+++ part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
@@ -1,21 +1,29 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class OnlyTheseNumbers {
-
+public class OnlyTheseNumbers{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == -1) {
+        
+        ArrayList<Integer> numbers = new ArrayList<> ();
+        
+        while(true) {
+            
+            Integer input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == -1) {
                 break;
-            }
-
-            numbers.add(number);
+            } else {
+                numbers.add(input);
+            }                        
         }
-
+        
+        System.out.println("From where?");
+        Integer startingIndex = Integer.valueOf(scanner.nextLine());
+        System.out.println("To where?");
+        Integer endIndex = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println(numbers.get(startingIndex));
+        System.out.println(numbers.get(endIndex));
     }
 }
