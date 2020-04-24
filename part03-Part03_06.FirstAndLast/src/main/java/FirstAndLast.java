65cb1421a18734fbb9e3e05d81a43dc766ace6ed
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-23 18:17:42 -0700

    Complete FirstAndLast

--- part03-Part03_06.FirstAndLast/src/main/java/FirstAndLast.java
+++ part03-Part03_06.FirstAndLast/src/main/java/FirstAndLast.java
@@ -1,21 +1,55 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class FirstAndLast {
-
+public class FirstAndLast{
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<String> list = new ArrayList<>();
-        while (true) {
+        
+        ArrayList<String> list = new ArrayList<> ();
+        
+        while(true) {
             String input = scanner.nextLine();
-            if (input.equals("")) {
+            
+            if(input.equals("")) {
                 break;
+            } else {
+                list.add(input);
             }
-
-            list.add(input);
         }
-
+        
+        int listLength = list.size();
+        
+        System.out.println(list.get(0));
+        System.out.println(list.get(listLength - 1));
     }
+
 }
+
+
+
+
+
+
+
+//
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+//public class FirstAndLast {
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        ArrayList<String> list = new ArrayList<>();
+//        while (true) {
+//            String input = scanner.nextLine();
+//            if (input.equals("")) {
+//                break;
+//            }
+//
+//            list.add(input);
+//        }
+//
+//    }
+//}
