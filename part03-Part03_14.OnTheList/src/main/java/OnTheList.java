d3e6f5318d94b98ed86756f073168c2dd4e3bc9b
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-25 11:55:27 -0700

    Complete OnTheList

--- part03-Part03_14.OnTheList/src/main/java/OnTheList.java
+++ part03-Part03_14.OnTheList/src/main/java/OnTheList.java
@@ -1,21 +1,30 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class OnTheList {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<String> list = new ArrayList<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
+        
+        ArrayList<String> names = new ArrayList<> ();
+        
+        while(true) {
+            String name = scanner.nextLine();
+            
+            if(name.equals("")) {
                 break;
+            } else {
+                names.add(name);
             }
-
-            list.add(input);
         }
-
+        
+        System.out.println("Search for?");
+        String query = scanner.nextLine();
+        
+        if(names.contains(query)) {
+            System.out.println(query + " was found!");
+        } else {
+            System.out.println(query + " was not found!");
+        }
+        
     }
 }
