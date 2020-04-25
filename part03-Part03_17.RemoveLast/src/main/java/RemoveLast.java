cb28c1f0327c371a03ce2b5bd92a8c8828c05450
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-25 12:44:23 -0700

    Complete RemoveLast

--- part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -4,7 +4,21 @@ import java.util.ArrayList;
 public class RemoveLast {
 
     public static void main(String[] args) {
-        // Try your method in here
+        
+        ArrayList<String> names = new ArrayList<> ();
+        
+        names.add("person");
+        names.add("person2");
+        names.add("person3");
+        removeLast(names);
+    }
+    
+    public static void removeLast(ArrayList<String> names) {
+        
+        if(names.size() > 0) {
+            names.remove(names.size() - 1);    
+        }
+        
     }
 
 }
