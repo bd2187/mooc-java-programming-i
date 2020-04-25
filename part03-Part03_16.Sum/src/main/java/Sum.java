1e6052edd8153068acec0b0fec6edabcc0d77907
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-25 12:21:00 -0700

    Complete Sum

--- part03-Part03_16.Sum/src/main/java/Sum.java
+++ part03-Part03_16.Sum/src/main/java/Sum.java
@@ -5,6 +5,23 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
+        
+        ArrayList<Integer> numbers = new ArrayList<> ();
+        numbers.add(10);
+        numbers.add(29);
+        numbers.add(6);
+        
+        sum(numbers);
+    }
+    
+    public static int sum(ArrayList<Integer> numbers) {
+        
+        int sum = 0;
+        for(Integer number: numbers) {
+            sum += number;
+        }
+                
+        return sum;
     }
 
 }
