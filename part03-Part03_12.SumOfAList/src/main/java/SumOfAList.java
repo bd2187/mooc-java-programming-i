0107965cc631f41f1bcb7542e5c6401b8cacce41
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-25 11:35:24 -0700

    COmplete SumOfAList

--- part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
+++ part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
@@ -1,24 +1,34 @@
+/*
+    Modify the program so that after reading the numbers
+    it calculates and prints the sum of the numbers in the list.
+*/
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class SumOfAList {
-
+public class SumOfAList { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<Integer> list = new ArrayList<>();
-        while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
-            if (input == -1) {
+        
+        ArrayList<Integer> numbers = new ArrayList<> ();
+        while(true) {
+            Integer number = Integer.valueOf(scanner.nextLine());
+            
+            if(number == -1) {
                 break;
+            } else {
+                
+                numbers.add(number);
+                
             }
-
-            list.add(input);
         }
-
-        System.out.println("");
-
-        // toteuta listan lukujen summan laskeminen tänne
+        
+        int sum = 0;
+        for(Integer number: numbers) {
+            sum += number;
+        }
+        
+        System.out.println("Sum: " + sum);
+        
     }
 }
