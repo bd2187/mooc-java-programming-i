ced40361b7ecd696816b153db6ebd4cb1ca10f19
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-28 22:47:18 -0700

    Complete AVClub

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -1,11 +1,26 @@
-
 import java.util.Scanner;
 
 public class AVClub {
-
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-
-
+        Boolean askUserForInput = true;
+        while(askUserForInput) {
+            
+            String input = scanner.nextLine();
+            
+            if(input.equals("")) {
+                askUserForInput = false;
+            } else {
+                
+                String[] arr = input.split(" ");
+                for(int i = 0 ; i < arr.length; i++) {
+                    if(arr[i].contains("av")) {
+                        System.out.println(arr[i]);
+                    }
+                }
+            }
+            
+        }
     }
 }
