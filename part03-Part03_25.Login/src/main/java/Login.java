1adc608b5fc6935460c5e75fd93b9928f03b77e1
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-28 22:17:10 -0700

    Complete Login

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -1,10 +1,23 @@
-
 import java.util.Scanner;
 
 public class Login {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Enter a username:");
+        String username = scanner.nextLine();
+        
+        System.out.println("Enter a password:");
+        String password = scanner.nextLine();
+        
+        if(
+            (username.equals("alex") && password.equals("sunshine")) ||
+            (username.equals("emma") && password.equals("haskell"))
+        ) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+        
     }
 }
