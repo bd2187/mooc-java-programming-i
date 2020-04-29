eec94364edb3dfe25d97dbbff1adeb86d04e3050
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-28 22:05:36 -0700

    Complete PrintThrice

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -6,7 +6,17 @@ public class PrintThrice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give a word:");
+        String input = scanner.nextLine();
+        
+        int index = 0;
+        String str = "";
+        while(index < 3) {
+            str += input;
+            index++;
+        }
+        
+        System.out.println(str);
 
     }
 }
