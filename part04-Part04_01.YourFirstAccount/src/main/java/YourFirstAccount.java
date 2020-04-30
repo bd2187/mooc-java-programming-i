f39ce7673eb0e62b5cb4322e0dc875688d2ee830
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-29 22:52:21 -0700

    Complete YourFirstAccount and YourFirstBankTransfer

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,10 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        Account myAccount = new Account("myAccount", 100.0);
+        
+        myAccount.deposit(20.0);
+        
+        System.out.println(myAccount);
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -1,8 +1,21 @@
+/*
+    Creates an account named "Matthews account" with the balance 1000
+    Creates an account named "My account" with the balance 0
+    Withdraws 100.0 from Matthew's account
+    Deposits 100.0 to "my account"
+    Prints both the accounts
+*/
 
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
+        Account matt = new Account("Matthews account", 1000);
+        Account myAccount = new Account("My account", 0);
+        
+        matt.withdrawal(100.0);
+        myAccount.deposit(100.0);
+        
+        System.out.println(matt);
+        System.out.println(myAccount);
     }
 }
