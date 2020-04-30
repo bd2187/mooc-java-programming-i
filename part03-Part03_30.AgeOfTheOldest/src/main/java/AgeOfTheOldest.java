b72f36774214e199330fd70ccb2c595663c902c1
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-29 21:56:55 -0700

    Complete AgeOFTheOldest and NameOfTheOldest

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -1,11 +1,46 @@
+/*
+    Write a program that reads names and ages from the user until an empty
+    line is entered. The name and age are separated by a comma.
+
+    After reading all user input, the program prints the age of the oldest person.
+    You can assume that the user enters at least one person, and that one of the
+    users is older than the others.
+*/
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AgeOfTheOldest {
-
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        Boolean askUser = true;        
+        ArrayList<String> namesList = new ArrayList<> ();
+        while(askUser) {
+            String input = scanner.nextLine();
+            
+            if(input.equals("")) {
+                askUser = false;
+            } else {
+                namesList.add(input);
+            }
+        }
+        
+        int ageOfOldest = 0;
+        for(int i = 0; i < namesList.size(); i++) {
+            String person = namesList.get(i);
+            String[] personArr = person.split(",");
+            int age = Integer.valueOf(personArr[1]);
+            String name = personArr[0];
+            
+            if(age > ageOfOldest) {
+                ageOfOldest = age;
+            }                        
+        }
+        
+        System.out.println("Name of the oldest: " + ageOfOldest);
+        
+        
     }
 }
--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -1,10 +1,40 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        Boolean askUser = true;        
+        ArrayList<String> namesList = new ArrayList<> ();
+        while(askUser) {
+            String input = scanner.nextLine();
+            
+            if(input.equals("")) {
+                askUser = false;
+            } else {
+                namesList.add(input);
+            }
+        }
+        
+        int ageOfOldest = 0;
+        String nameOfTheOldest = "";
+        for(int i = 0; i < namesList.size(); i++) {
+            String person = namesList.get(i);
+            String[] personArr = person.split(",");
+            int age = Integer.valueOf(personArr[1]);
+            String name = personArr[0];
+            
+            if(age > ageOfOldest) {
+                ageOfOldest = age;
+                nameOfTheOldest = name;
+            }                        
+        }
+        
+        System.out.println("Name of the oldest: " + nameOfTheOldest);
+ 
 
 
     }
