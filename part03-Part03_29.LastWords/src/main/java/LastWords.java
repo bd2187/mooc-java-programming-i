5125bb8b0748ad385c4b368737a81be2dd7a5f63
bd2187 <brandon.dionisio.26@gmail.com>
2020-04-29 21:28:38 -0700

    Complete LastWords

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,19 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        Boolean askUser = true;
+        
+        while(askUser) {
+            String input = scanner.nextLine();
+            
+            if(input.equals("")) {
+                askUser = false;
+            } else {
+                String[] arr = input.split(" ");
+                System.out.println((arr[arr.length - 1]));
+            }
+        }
 
     }
 }
