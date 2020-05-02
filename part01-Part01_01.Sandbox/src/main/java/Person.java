d894633a3995cdd5bc8a6bcde6bebb2b3686afce
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-02 15:57:44 -0700

    Add Person constructor to Person class

--- part01-Part01_01.Sandbox/src/main/java/Person.java
+++ part01-Part01_01.Sandbox/src/main/java/Person.java
@@ -1,4 +1,9 @@
 public class Person {
     private String name;
     private int age;
+    
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
 }
