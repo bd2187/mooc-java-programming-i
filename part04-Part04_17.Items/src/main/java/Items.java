3f31771d3facbd7cd4bd50b7e47f6f4807a1941d
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-04 17:06:06 -0700

    Complete Items

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -5,10 +5,27 @@ import java.util.Scanner;
 public class Items {
 
     public static void main(String[] args) {
-        // implement here your program that uses the class Item
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        Boolean askForInput = true;
+        
+        while(askForInput) {
+            System.out.println("Name:");
+
+            String input = scanner.nextLine();
+            
+            if(input.isEmpty()) {
+                break;
+            } else {
+                items.add(new Item(input));
+            }
+        }
+        
+        for(int i = 0; i < items.size(); i++) {
+            System.out.println(items.get(i).toString());
+        }
 
     }
 }
