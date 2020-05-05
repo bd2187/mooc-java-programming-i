1afb728a29643fc76ae00c93825acea64af36e45
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-04 17:31:42 -0700

    Complete TelevisionProgram

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -4,10 +4,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // implement here your program that uses the TelevisionProgram class
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        Boolean askUser = true;
+        while(askUser) {
+        
+            System.out.println("Name:");
+            
+            String name = scanner.nextLine();
+            if(name.isEmpty()) {
+                break;
+            } else {
+                System.out.println("Duration:");
+                int duration = Integer.valueOf(scanner.nextLine());
+                programs.add(new TelevisionProgram(name, duration));                
+            }
+        }
+        
+        System.out.println("Program's maximum duration?");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        
+        for(int i = 0; i < programs.size(); i++) {
+            
+            TelevisionProgram program = programs.get(i);
+            
+            if(program.getDuration() <= maxDuration) {
+                System.out.println(program.toString());
+            }
+            
+        }
 
     }
 }
