18748e1b9c7d033a360e1cdc8847b6d3f3308dd9
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-04 22:00:55 -0700

    Complete Cubes

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,22 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        Boolean askUser = true;
+        
+        while(askUser) {
+            
+            String input = scanner.nextLine();
+            
+            if(input.equals("end")) {
+                break;
+            }
+            
+            int num = Integer.valueOf(input);
+            int cube = num * num * num;
+            
+            System.out.println(cube);
+            
+        }
     }
 }
