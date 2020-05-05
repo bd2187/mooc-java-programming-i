20bd61b1708df316d3fa9b048314ef66af5b7d1f
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-04 17:17:54 -0700

    Complete PersonalInformationCollection

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,31 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        Boolean askUser = true;
+        
+        while(askUser) {
+            
+            System.out.println("First name:");
+            String firstName = scanner.nextLine();
+            
+            if(firstName.isEmpty()) {
+                break;
+            } else {
+                System.out.println("Last name:");
+                String lastName = scanner.nextLine();
+                
+                System.out.println("Identification number");
+                String id = scanner.nextLine();
+                
+                infoCollection.add(new PersonalInformation(firstName, lastName, id));
+            }
+        }
+        
+        for(int i = 0; i < infoCollection.size(); i++) {
+            PersonalInformation info = infoCollection.get(i);            
+            System.out.println(info.getFirstName() + " " + info.getLastName());
+        }
 
     }
 }
