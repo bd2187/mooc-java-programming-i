245d19d11465d1c30ea8d3c5418c6fba13949e30
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-04 21:55:40 -0700

    Complete NumberOfStrings

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,21 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = 0;
+        
+        Boolean askUser = true;
+        while(askUser) {
+            String input = scanner.nextLine();    
+            
+            if(input.equals("end")) {
+                break;
+            }
+            
+            count++;
+        }
+        
+        System.out.println(count);
+        
+        
     }
 }
