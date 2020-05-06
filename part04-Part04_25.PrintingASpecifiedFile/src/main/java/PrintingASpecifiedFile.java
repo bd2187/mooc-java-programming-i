b23e32b6f26b4769f2a518ad46fc69cdcb208fa1
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-05 21:15:51 -0700

    Complete PrintingASpecifiedFile

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,22 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Which file should have its contents printed?");
+        
+        String input = scanner.nextLine();
+        
+        try(Scanner file = new Scanner(Paths.get(input))) {
+            
+            while(file.hasNextLine()) {
+                String row = file.nextLine();            
+                System.out.println(row);                
+            }
+
+            
+        } catch(Exception err) {
+            System.out.println(err.getMessage());
+        }
 
     }
 }
