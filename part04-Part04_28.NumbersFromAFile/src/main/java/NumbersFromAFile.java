a6f7b69ceaa39ba62a0fbfc4504bc837de4b3b61
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-05 21:42:21 -0700

    Complete NumbersFromAFile

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,19 +1,33 @@
-
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("File? ");
-        String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
+        
+        System.out.println("File?");
+        String fileName = scanner.nextLine();
+        
+        System.out.println("Lower bound?");
         int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
+        System.out.println("Upper bound?");
         int upperBound = Integer.valueOf(scanner.nextLine());
-
+        
+        int count = 0;
+        try(Scanner fileReader = new Scanner(Paths.get(fileName))) {
+            
+            while(fileReader.hasNextLine()) {
+               int number = Integer.valueOf(fileReader.nextLine());
+               
+               if(number >= lowerBound && number <= upperBound) {
+                   count++;
+               }
+            }
+            
+        } catch(Exception err) {
+            
+        }
+        
+        System.out.println("Numbers: " + count);
     }
-
 }
