477fda5338083406d3d5d693da025dc1c1637e01
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-05 22:08:43 -0700

    Complete RecordsFromAFile

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,11 +1,34 @@
-
-import java.nio.file.Paths;
 import java.util.Scanner;
+import java.nio.file.Paths;
 
 public class RecordsFromAFile {
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Name of the file:");
+        
+        String fileName = scanner.nextLine();
+        
+        try(Scanner fileReader = new Scanner(Paths.get(fileName))){
+            
+            while(fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] lineArr = line.split(",");
+                String name = lineArr[0];
+                int age = Integer.valueOf(lineArr[1]);
+                
+                String years = "years";
+                
+                if(age == 1) {
+                    years = "year";
+                }
+                
+                
+                System.out.println(name + ", age: " + age + " " + years);
+            }
+            
+        } catch(Exception err) {
+            
+        }
     }
 }
