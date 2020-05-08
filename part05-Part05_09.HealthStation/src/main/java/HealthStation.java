2f21098dbd08aabdbb9c495b71266d469751a1f3
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-07 22:13:16 -0700

    Complete HealthStation

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,20 @@
 
 public class HealthStation {
 
+    private int weighings;
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        this.weighings++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        int newWeight = person.getWeight() + 1;
+        person.setWeight(newWeight);
+    }
+    
+    public int weighings() {
+        return this.weighings;
     }
 
 }
