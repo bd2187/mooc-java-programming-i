d7fb81aa54a8a22581656b7bcee8327ae6a9eea7
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-11 22:25:16 -0700

    Complete Apartment

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,45 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
+    }
+    
+    public boolean largerThan(Apartment compared) {
+        if(this.squares > compared.squares) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public int priceDifference(Apartment compared) {
+        
+        int priceOfApartment = this.squares * this.pricePerSquare;
+        int priceOfCompared = compared.squares * compared.pricePerSquare;
+        
+        int diff_1 = priceOfApartment - priceOfCompared;
+        
+        if(diff_1 >= 0) {
+            return diff_1;
+        } else {
+            return priceOfCompared - priceOfApartment;
+        }               
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        int priceOfApartment = this.squares * this.pricePerSquare;
+        int priceOfCompared = compared.squares * compared.pricePerSquare;
+
+        if(priceOfCompared < priceOfApartment) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
