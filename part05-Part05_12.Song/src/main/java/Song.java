660ec915f10174d4c83d129f71159e50fc72d406
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-11 22:50:00 -0700

    Complete Song

--- part05-Part05_12.Song/src/main/java/Song.java
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -10,6 +10,27 @@ public class Song {
         this.name = name;
         this.durationInSeconds = durationInSeconds;
     }
+    
+    public boolean equals(Object compared) {
+        if(this == compared) {
+            return true;
+        }
+        
+        if(!(compared instanceof Song)) {
+            return false;
+        }
+        
+        Song comparedSong = (Song) compared;
+        
+        if(this.artist == comparedSong.artist &&
+            this.name == comparedSong.name &&
+            this.durationInSeconds == comparedSong.durationInSeconds) {
+            
+            return true;            
+        }
+        
+        return false;
+    }
 
     @Override
     public String toString() {
