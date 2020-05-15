3c86af42c93a12712ef800cc7e6837496ecadb50
bd2187 <brandon.dionisio.26@gmail.com>
2020-05-14 18:07:57 -0700

    Complete money

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -22,6 +22,47 @@ public class Money {
     public int cents() {
         return this.cents;
     }
+    
+    public Money plus(Money addition) {
+          int newEuros = this.euros() + addition.euros();
+          int newCents = this.cents() + addition.cents();
+        
+        Money newMoney = new Money(newEuros, newCents);               
+        
+        return newMoney;
+    }
+    
+    public Money minus(Money decreaser) {
+        
+         double aMoney = (double) this.euros() + (this.cents() / 100);
+         double bMoney = (double) decreaser.euros() + (decreaser.cents() / 100);
+         
+         
+         double cMoney = aMoney - bMoney;
+         
+         String[] arr = String.valueOf(cMoney).split("\\.");
+         
+         int firstInt = Integer.parseInt(arr[0]);
+         int secondInt = Integer.parseInt(arr[1]);
+
+
+       Money newMoney = new Money(firstInt, secondInt);
+       
+       return newMoney;
+    }
+    
+    public boolean lessThan(Money compared) {
+        if(this.euros() < compared.euros()) {
+            return true;
+        }
+        
+        if(this.euros() == compared.euros() && this.cents() < compared.cents()) {
+            return true;
+        }
+        
+        return false;
+    }    
+    
 
     public String toString() {
         String zero = "";
